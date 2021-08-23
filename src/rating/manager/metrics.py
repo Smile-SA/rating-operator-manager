b0210578718eaa132016130e9993f33c8fe2eb57
Hong-Phuc Vu <hong-phuc.vu@alterway.fr>
2021-08-23 16:25:14 +0200

    v2.0.0 updated files
    Signed-off-by: Hong-Phuc Vu <hong-phuc.vu@alterway.fr>

--- src/rating/manager/metrics.py
+++ src/rating/manager/metrics.py
@@ -11,32 +11,32 @@ def ensure_metrics_config(config: Dict) -> Dict:
     metrics = []
     for metric, conf in config.items():
         if metric in metrics:
-            raise utils.ConfigurationException(
+            raise utils.ConfigurationExceptionError(
                 'Duplicated key in metrics definition', metric)
         metrics.append(metric)
 
         keys = set(conf.keys())
         if len(keys) < 4:
-            raise utils.ConfigurationException(
+            raise utils.ConfigurationExceptionError(
                 'Missing key in metrics definition', keys
             )
 
         accepted_keys = {'report_name', 'presto_table', 'presto_column', 'unit'}
         if keys != accepted_keys:
-            raise utils.ConfigurationException(
+            raise utils.ConfigurationExceptionError(
                 'Unsupported key in metrics definition', keys
             )
 
         for key in conf.values():
             if not utils.is_valid_against(key, '^[a-zA-Z0-9-_]+$'):
-                raise utils.ConfigurationException(
+                raise utils.ConfigurationExceptionError(
                     'Invalid value in metrics definition',
                     key
                 )
 
         unit = conf['unit']
         if unit not in ['core-seconds', 'byte-seconds', 'byte']:
-            raise utils.ConfigurationException(
+            raise utils.ConfigurationExceptionError(
                 'Unsupported unit in metrics definition',
                 unit
             )
