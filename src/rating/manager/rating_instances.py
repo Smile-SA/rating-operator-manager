1c1439c05d45300c33c67d7a7fc151be62ae24a3
Boumedienne MAROUF <boumedienne.marouf@smile.fr>
2024-06-28 08:03:53 +0000

    Updating namespaces for events

--- src/rating/manager/rating_instances.py
+++ src/rating/manager/rating_instances.py
@@ -8,22 +8,44 @@ from datetime import datetime as dt
 
 from rating.manager import utils
 
+
+@kopf.on.create('rating.alterway.fr', 'v1', 'ratingruleinstances')
+@kopf.on.update('rating.alterway.fr', 'v1', 'ratingruleinstances')
+@utils.assert_rating_namespace
+def rating_instances_creation_alterway(body: Dict,
+                                       spec: Dict,
+                                       logger: Logger,
+                                       **kwargs: Dict):
+    handle_rating_instances_creation(body, spec, logger, **kwargs)
+
 @kopf.on.create('rating.smile.fr', 'v1', 'ratingruleinstances')
 @kopf.on.update('rating.smile.fr', 'v1', 'ratingruleinstances')
 @utils.assert_rating_namespace
-def rating_instances_creation(body: Dict,
-                              spec: Dict,
-                              logger: Logger,
-                              **kwargs: Dict):
-    """
-    Create values of RatingRuleInstances through rating-api after creation in kubernetes.
+def rating_instances_creation_smile(body: Dict,
+                                    spec: Dict,
+                                    logger: Logger,
+                                    **kwargs: Dict):
+    handle_rating_instances_creation(body, spec, logger, **kwargs)
 
-    :body (Dict) A dictionary containing the created kubernetes object.
-    :spec (Dict) A smaller version of body.
-    :logger (Logger) A Logger object to log informations.
-    :kwargs (Dict) A dictionary holding unused parameters.
+def handle_rating_instances_creation(body: Dict,
+                                     spec: Dict,
+                                     logger: Logger,
+                                     **kwargs: Dict):
     """
-    if 'cpu' in spec.keys() and 'memory' in spec.keys() and 'price' in spec.keys() and 'surface' in spec.keys() and 'power' in spec.keys():
+    Create values of RatingRuleInstances through rating-api after creation in Kubernetes.
+
+    :param body: A dictionary containing the created Kubernetes object.
+    :type body: Dict
+    :param spec: A smaller version of body.
+    :type spec: Dict
+    :param logger: A Logger object to log information.
+    :type logger: Logger
+    :param kwargs: A dictionary holding unused parameters.
+    :type kwargs: Dict
+    """
+    required_keys = ['cpu', 'memory', 'price']
+    
+    if all(key in spec for key in required_keys):
         rules_name = body['metadata']['name']
         data = {
             'metric_name': spec.get('name', {}),
@@ -31,51 +53,63 @@ def rating_instances_creation(body: Dict,
             'promql': spec.get('metric', {}),
             'cpu': spec.get('cpu', {}),
             'memory': spec.get('memory', {}),
-            'price': spec.get('price', {}),
-            'power': spec.get('power', {}),
-            'surface': spec.get('surface', {})
+            'price': spec.get('price', {})
         }
         try:
-            utils.post_for_rating_api(endpoint='/templates/metric/add',
-                                      payload=data)
-            utils.post_for_rating_api(endpoint='/templates/instance/add',payload=data)
+            utils.post_for_rating_api(endpoint='/templates/metric/add', payload=data)
+            utils.post_for_rating_api(endpoint='/templates/instance/add', payload=data)
         except utils.ConfigurationExceptionError as exc:
             logger.error(f'RatingRulesInstance {rules_name} is invalid. Reason: {exc}')
         except requests.exceptions.RequestException:
             logger.error(f'Request for RatingRulesInstance {rules_name} update failed')
         else:
-            logger.info(
-                f'RatingRule {rules_name} created/updated.')
-
+            logger.info(f'RatingRule {rules_name} created/updated.')
 
 @kopf.on.delete('rating.smile.fr', 'v1', 'ratingruleinstances')
 @utils.assert_rating_namespace
-def rating_instances_deletion(body: Dict,
-                              spec: Dict,
-                              logger: Logger,
-                              **kwargs: Dict):
-    """
-    Delete values of RatingRuleInstances through rating-api after creation in kubernetes.
+def rating_instances_deletion_smile(body: Dict,
+                                    spec: Dict,
+                                    logger: Logger,
+                                    **kwargs: Dict):
+    handle_rating_instances_deletion(body, spec, logger, **kwargs)
 
-    :body (Dict) A dictionary containing the deleted kubernetes object.
-    :spec (Dict) A smaller version of body.
-    :logger (Logger) A Logger object to log informations.
-    :kwargs (Dict) A dictionary holding unused parameters.
+@kopf.on.delete('rating.alterway.fr', 'v1', 'ratingruleinstances')
+@utils.assert_rating_namespace
+def rating_instances_deletion_alterway(body: Dict,
+                                       spec: Dict,
+                                       logger: Logger,
+                                       **kwargs: Dict):
+    handle_rating_instances_deletion(body, spec, logger, **kwargs)
+
+def handle_rating_instances_deletion(body: Dict,
+                                     spec: Dict,
+                                     logger: Logger,
+                                     **kwargs: Dict):
     """
-    if 'cpu' in spec.keys() and 'memory' in spec.keys() and 'price' in spec.keys():
+    Delete values of RatingRuleInstances through rating-api after deletion in Kubernetes.
+
+    :param body: A dictionary containing the deleted Kubernetes object.
+    :type body: Dict
+    :param spec: A smaller version of body.
+    :type spec: Dict
+    :param logger: A Logger object to log information.
+    :type logger: Logger
+    :param kwargs: A dictionary holding unused parameters.
+    :type kwargs: Dict
+    """
+    required_keys = ['cpu', 'memory', 'price']
+
+    if all(key in spec for key in required_keys):
         rules_name = body['metadata']['name']
         data = {
             'metric_name': spec.get('name', {}),
         }
         try:
-            utils.post_for_rating_api(endpoint='/templates/metric/delete',
-                                      payload=data)
-            utils.post_for_rating_api(endpoint='/templates/instance/delete',
-                                      payload=data)
+            utils.post_for_rating_api(endpoint='/templates/metric/delete', payload=data)
+            utils.post_for_rating_api(endpoint='/templates/instance/delete', payload=data)
         except utils.ConfigurationExceptionError as exc:
             logger.error(f'RatingRulesInstance {rules_name} is invalid. Reason: {exc}')
         except requests.exceptions.RequestException:
             logger.error(f'Request for RatingRulesInstance {rules_name} delete failed')
         else:
-            logger.info(
-                f'RatingRule {rules_name} deleted.')
+            logger.info(f'RatingRule {rules_name} deleted.')
--- src/rating/manager/rating_rules.py
+++ src/rating/manager/rating_rules.py
@@ -9,20 +9,17 @@ from datetime import datetime as dt
 from rating.manager import utils
 
 
+@kopf.on.create('rating.alterway.fr', 'v1', 'ratingrules')
+@utils.assert_rating_namespace
+def rating_rules_creation_alterway(body: Dict, spec: Dict, logger: Logger, **kwargs: Dict):
+    handle_rating_rules_creation(body, spec, logger, **kwargs)
+
 @kopf.on.create('rating.smile.fr', 'v1', 'ratingrules')
 @utils.assert_rating_namespace
-def rating_rules_creation(body: Dict,
-                          spec: Dict,
-                          logger: Logger,
-                          **kwargs: Dict):
-    """
-    Create and validate the RatingRules through rating-api after creation in kubernetes.
+def rating_rules_creation_smile(body: Dict, spec: Dict, logger: Logger, **kwargs: Dict):
+    handle_rating_rules_creation(body, spec, logger, **kwargs)
 
-    :body (Dict) A dictionary containing the created kubernetes object.
-    :spec (Dict) A smaller version of body.
-    :logger (Logger) A Logger object to log informations.
-    :kwargs (Dict) A dictionary holding unused parameters.
-    """
+def handle_rating_rules_creation(body: Dict, spec: Dict, logger: Logger, **kwargs: Dict):
     timestamp = body['metadata']['creationTimestamp']
     rules_name = body['metadata']['name']
     data = {
@@ -31,31 +28,25 @@ def rating_rules_creation(body: Dict,
         'timestamp': timestamp
     }
     try:
-        utils.post_for_rating_api(endpoint='/ratingrules/add',
-                                  payload=data)
+        utils.post_for_rating_api(endpoint='/ratingrules/add', payload=data)
     except utils.ConfigurationExceptionError as exc:
         logger.error(f'RatingRules {rules_name} is invalid. Reason: {exc}')
     except requests.exceptions.RequestException:
         raise kopf.TemporaryError(f'Request for RatingRules {rules_name} update failed. retrying in 30s', delay=30)
     else:
-        logger.info(
-            f'RatingRule {rules_name} created, valid from {timestamp}.')
+        logger.info(f'RatingRule {rules_name} created, valid from {timestamp}.')
 
+@kopf.on.update('rating.alterway.fr', 'v1', 'ratingrules')
+@utils.assert_rating_namespace
+def rating_rules_update_alterway(body: Dict, spec: Dict, logger: Logger, **kwargs: Dict):
+    handle_rating_rules_update(body, spec, logger, **kwargs)
 
 @kopf.on.update('rating.smile.fr', 'v1', 'ratingrules')
 @utils.assert_rating_namespace
-def rating_rules_update(body: Dict,
-                        spec: Dict,
-                        logger: Logger,
-                        **kwargs: Dict):
-    """
-    Update and validate the RatingRules through rating-api after update in kubernetes.
+def rating_rules_update_smile(body: Dict, spec: Dict, logger: Logger, **kwargs: Dict):
+    handle_rating_rules_update(body, spec, logger, **kwargs)
 
-    :body (Dict) A dictionary containing the updated kubernetes object.
-    :spec (Dict) A smaller version of body.
-    :logger (Logger) A Logger object to log informations.
-    :kwargs (Dict) A dictionary holding unused parameters.
-    """
+def handle_rating_rules_update(body: Dict, spec: Dict, logger: Logger, **kwargs: Dict):
     timestamp = body['metadata']['creationTimestamp']
     rules_name = body['metadata']['name']
     data = {
@@ -64,8 +55,7 @@ def rating_rules_update(body: Dict,
         'timestamp': timestamp
     }
     try:
-        utils.post_for_rating_api(endpoint='/ratingrules/update',
-                                  payload=data)
+        utils.post_for_rating_api(endpoint='/ratingrules/update', payload=data)
     except utils.ApiExceptionError:
         logger.warning(f'RatingRules {rules_name} does not exist in storage, ignoring.')
     except utils.ConfigurationExceptionError as exc:
@@ -76,29 +66,24 @@ def rating_rules_update(body: Dict,
         logger.info(f'Rating rules {rules_name} was updated.')
 
 
+@kopf.on.delete('rating.alterway.fr', 'v1', 'ratingrules')
+@utils.assert_rating_namespace
+def rating_rules_deletion_alterway(body: Dict, spec: Dict, logger: Logger, **kwargs: Dict):
+    handle_rating_rules_deletion(body, spec, logger, **kwargs)
+
 @kopf.on.delete('rating.smile.fr', 'v1', 'ratingrules')
 @utils.assert_rating_namespace
-def rating_rules_deletion(body: Dict,
-                          spec: Dict,
-                          logger: Logger,
-                          **kwargs: Dict):
-    """
-    Delete the RatingRules through rating-api after deletion in kubernetes.
+def rating_rules_deletion_smile(body: Dict, spec: Dict, logger: Logger, **kwargs: Dict):
+    handle_rating_rules_deletion(body, spec, logger, **kwargs)
 
-    :body (Dict) A dictionary containing the deleted kubernetes object.
-    :spec (Dict) A smaller version of body.
-    :logger (Logger) A Logger object to log informations.
-    :kwargs (Dict) A dictionary holding unused parameters.
-    """
+def handle_rating_rules_deletion(body: Dict, spec: Dict, logger: Logger, **kwargs: Dict):
     timestamp = body['metadata']['creationTimestamp']
     rules_name = body['metadata']['name']
     data = {
         'timestamp': int(dt.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ').timestamp())
     }
-
     try:
-        utils.post_for_rating_api(endpoint='/ratingrules/delete',
-                                  payload=data)
+        utils.post_for_rating_api(endpoint='/ratingrules/delete', payload=data)
     except utils.ApiExceptionError:
         logger.warning(f'RatingRules {rules_name} does not exist in storage, ignoring.')
     except requests.exceptions.RequestException:
@@ -106,25 +91,20 @@ def rating_rules_deletion(body: Dict,
     else:
         logger.info(f'RatingRules {rules_name} ({timestamp}) was deleted.')
 
-
 @kopf.on.delete('rating.smile.fr', 'v1', 'ratedmetrics')
 @utils.assert_rating_namespace
-def delete_rated_metric(body: Dict,
-                        spec: Dict,
-                        logger: Logger,
-                        **kwargs: Dict):
-    """
-    Delete the data associated with the ratedMetrics through the rating-api.
+def delete_rated_metric_smile(body: Dict, spec: Dict, logger: Logger, **kwargs: Dict):
+    handle_delete_rated_metric(body, spec, logger, **kwargs)
 
-    :body (Dict) A dictionary containing the deleted kubernetes object.
-    :spec (Dict) A smaller version of body.
-    :logger (Logger) A Logger object to log informations.
-    :kwargs (Dict) A dictionary holding unused parameters.
-    """
+@kopf.on.delete('rating.alterway.fr', 'v1', 'ratedmetrics')
+@utils.assert_rating_namespace
+def delete_rated_metric_alterway(body: Dict, spec: Dict, logger: Logger, **kwargs: Dict):
+    handle_delete_rated_metric(body, spec, logger, **kwargs)
+
+def handle_delete_rated_metric(body: Dict, spec: Dict, logger: Logger, **kwargs: Dict):
     data = {
         'metric': spec['metric']
     }
-    response = utils.post_for_rating_api(endpoint='/rated/frames/delete',
-                                         payload=data)
+    response = utils.post_for_rating_api(endpoint='/rated/frames/delete', payload=data)
     if response:
         logger.info(f'deleted {response["results"]} rows associated with {body["metadata"]["name"]}')
